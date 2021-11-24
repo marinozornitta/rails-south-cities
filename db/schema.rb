f97bc34caff224684ce19dff03118023cba3e05a
marinozornitta <marino.zornitta@gmail.com>
2021-11-24 17:30:00 -0300

    Updated schema.rb
    Part of "Creation of City through Scaffold" commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_181814) do
+ActiveRecord::Schema.define(version: 2021_11_24_195203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.bigint "state_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["state_id"], name: "index_cities_on_state_id"
+  end
 
   create_table "states", force: :cascade do |t|
     t.string "name"
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_11_24_181814) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cities", "states"
 end
