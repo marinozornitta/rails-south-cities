dfa845cc79c7c43e4d1cefa69d240960cf9150aa
marinozornitta <marino.zornitta@gmail.com>
2021-11-28 10:02:02 -0300

    Routing root to Cities Index

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :cities
   resources :states
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'cities#index'
 end
