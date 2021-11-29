da79dbae43d313d635418eea4a517179e630009c
marinozornitta <marino.zornitta@gmail.com>
2021-11-28 22:32:48 -0300

    Added presence validation to models

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,6 +2,7 @@
 
 class City < ApplicationRecord
   belongs_to :state
+  validates :name, presence: true
 
   scope :filter_by_state_name, ->(state) { joins(:state).where('lower(states.name) like ?', "%#{state.downcase}%") }
   scope :filter_by_city_name_like, ->(name) { where('lower(cities.name) LIKE ?', "%#{name.downcase}%") }
